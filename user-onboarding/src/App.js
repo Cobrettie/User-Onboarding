ef390bcc13ca038decef0bbd36300aee9a81f5ac
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-19 21:33:56 -0400

    successful post to api

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import LoginForm from './Components/LoginForm';
+import FormikLoginForm from './Components/LoginForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Using Formik, Yup, Validation, Post Requests</h1>
-      <LoginForm />
+      <FormikLoginForm />
     </div>
   );
 }
--- user-onboarding/src/Components/LoginForm.js
+++ user-onboarding/src/Components/LoginForm.js
@@ -3,38 +3,47 @@ import { withFormik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
 
-function LoginForm() {
+// Adding destructured 'errors' prop to the form. The 'errors' prop gets passed down from the 'withFormik' component.
+function LoginForm({ values, errors, touched }) {
     return (
-        <div>
-            <h1>Login Form</h1>
-            <Form>
+        <Form>
+            <div>
+                {touched.name && errors.name && <p>{errors.name}</p>}
                 <Field
-                    type="name"
+                    type="text"
                     name="name"
                     placeholder="Name"
                 />
+            </div>
+            <div>
+                {touched.email && errors.email && <p>{errors.email}</p>}
                 <Field
                     type="email"
                     name="email"
                     placeholder="Email"
                 />
+            </div>
+            <div>
+                {touched.password && errors.password && <p>{errors.password}</p>}
                 <Field 
                     type="password"
                     name="password"
                     placeholder="Password"
                 />
-                <label>
-                    <Field 
-                        type="checkbox"
-                        name="tos"
-                        checked={values.tos}
-                    />
-                    {console.log(values.tos)}
-                    Accept TOS
-                </label>
-                <button>Submit Form</button>
-            </Form> 
-        </div>
+            </div>
+            {touched.tos && errors.tos && <p>{errors.tos}</p>}
+            <label>
+                <Field 
+                    type="checkbox"
+                    name="tos"
+                    value="false"
+                    // checked={values.tos}
+                />
+                {/* {console.log(values.tos)} */}
+                Accept TOS
+            </label>
+            <button type="submit">Submit Form</button>
+        </Form> 
     )
 }
 
@@ -44,9 +53,41 @@ const FormikLoginForm = withFormik({
             name: name || "",
             email: email || "",
             password: password || "",
-            tos: tos || false
+            tos: tos || "false"
         }
-    }
-})
+    },
 
-export default LoginForm
+    // VALIDATION SCHEMA
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+        .required("Name is required to submit form"),
+        email: Yup.string()
+        .email("Email not valid")
+        .required("Email is required to submit form"),
+        password: Yup.string()
+        .min(6, "Password must be 6 characters or longer")
+        .required("Password is required to submit form"),
+        tos: Yup.boolean()
+        .oneOf([true], "Please accept our TOS before submitting your form")
+    }),
+    // END VALIDATION SCHEMA
+
+    // create submit handler 
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        console.log(values)
+
+        axios
+            .post("https://reqres.in/api/users", values)
+            .then(res => {
+                console.log(res)
+                resetForm()
+                setSubmitting(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setSubmitting(false)
+            })
+    }
+})(LoginForm)
+
+export default FormikLoginForm
